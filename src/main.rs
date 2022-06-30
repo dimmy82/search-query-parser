456fd54e629db33ea4b0de5beeeb31c0ab75eb53
dimmy82 <chengdimmy82@yahoo.co.jp>
2022-07-01 02:51:49 +0900

    check Negative Brackets in layered_by_bracket function

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@ pub struct LayeredQueries(Vec<LayeredQuery>);
 pub enum LayeredQuery {
     Query(Query),
     Bracket(LayeredQueries),
+    NegativeBracket(LayeredQueries),
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -22,8 +23,13 @@ pub enum Condition {
     Keyword(String),
     ExactKeyword(String),
     Not(Box<Condition>),
-    And(Vec<Condition>),
-    Or(Vec<Condition>),
+    Operator(Operator, Vec<Condition>),
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum Operator {
+    And,
+    Or,
 }
 
 impl Query {
@@ -42,7 +48,7 @@ impl Query {
     }
 
     fn is_not_blank(&self) -> bool {
-        self.0.replace(" ", "").is_empty() == false
+        self.value().replace(" ", "").is_empty() == false
     }
 
     fn layered_by_bracket(self) -> Result<LayeredQueries> {
@@ -75,49 +81,73 @@ impl Query {
         }
 
         let mut bracket_queries = Vec::<Query>::new();
-        let all_brackets_removed_query = pick_layer_by_bracket(self.0, &mut bracket_queries)?;
+        let all_brackets_replace_query = pick_layer_by_bracket(self.0, &mut bracket_queries)?;
 
         fn combine_layered_query(
             query: Query, bracket_queries: &Vec<Query>,
         ) -> Result<LayeredQueries> {
-            let regex_layered_by_bracket = Regex::new(r"([^\(\)]*)\((\d)\)([^\(\)]*)")?;
+            let regex_layered_by_bracket = Regex::new(r"([^\(\)]*)\((\d)\)")?;
             let mut layered_queries = Vec::<LayeredQuery>::new();
-            regex_layered_by_bracket
-                .captures_iter(query.value())
-                .for_each(|captures| {
-                    filter_not_blank_query(captures.get(1))
-                        .map(|q| layered_queries.push(LayeredQuery::Query(q)));
+            let the_last_query_after_all_brackets = regex_layered_by_bracket
+                .replace_all(query.value(), |captures: &Captures| {
+                    let mut is_negative_bracket = false;
+                    filter_not_blank_query(captures.get(1)).map(|mut q| {
+                        if q.value().ends_with("-") {
+                            is_negative_bracket = true;
+                            q = Query::new(String::from(&q.value()[0..q.value().len() - 1]))
+                        }
+                        if q.is_not_blank() {
+                            layered_queries.push(LayeredQuery::Query(q))
+                        }
+                    });
                     captures
                         .get(2)
                         .map(|m| m.as_str().parse::<usize>())
                         .map(|index| {
                             index.map(|i| {
                                 bracket_queries.get(i - 1).map(|q: &Query| {
-                                    combine_layered_query(q.clone(), bracket_queries)
-                                        .map(|v| layered_queries.push(LayeredQuery::Bracket(v)))
+                                    combine_layered_query(q.clone(), bracket_queries).map(|v| {
+                                        layered_queries.push(if is_negative_bracket {
+                                            LayeredQuery::NegativeBracket(v)
+                                        } else {
+                                            LayeredQuery::Bracket(v)
+                                        })
+                                    })
                                 })
                             })
                         });
-                    filter_not_blank_query(captures.get(3))
-                        .map(|q| layered_queries.push(LayeredQuery::Query(q)));
-                });
-            if layered_queries.is_empty() {
-                layered_queries.push(LayeredQuery::Query(query))
+                    String::from("")
+                })
+                .to_string();
+            let the_last_query = Query::new(the_last_query_after_all_brackets);
+            if the_last_query.is_not_blank() {
+                layered_queries.push(LayeredQuery::Query(the_last_query))
             }
             Ok(LayeredQueries(layered_queries))
         }
 
         Ok(combine_layered_query(
-            Query::new(all_brackets_removed_query),
+            Query::new(all_brackets_replace_query),
             &bracket_queries,
         )?)
+    }
+
+    fn parse_to_condition(self) -> Result<Condition> {
+        unimplemented!()
     }
 }
 
 impl LayeredQueries {
     // k1 or (k2 and (-k3 or -k4))
-    fn parse(self) -> Result<Condition> {
-        self.0.into_iter().for_each(|layer| {});
+    fn parse_to_condition(self) -> Result<Condition> {
+        let layered_query_count = self.0.iter().count();
+        self.0
+            .into_iter()
+            .enumerate()
+            .for_each(|(index, layered_query)| match index {
+                i if i < layered_query_count - 1 => {}
+                _ => {}
+            });
         unimplemented!()
     }
 }
@@ -129,17 +159,17 @@ mod tests {
     #[test]
     fn test_replace_full_width_bracket_quotation_and_space_when_new() {
         let target =
-            Query::new("　ＡＡＡ　（”１１１　ＣＣＣ”　（（　ＤＤＤ　エエエ　）　ＦＦＦ）　ＧＧＧ　（ＨＨＨ　”あああ　いいい”　ううう））　”　ＪＪＪ　”　（ＫＫＫ　（　）　ＬＬＬ）　　（ＭＭＭ）　２２２　".into());
+            Query::new("　ＡＡＡ　（”１１１　ＣＣＣ”　（-（　ＤＤＤ　エエエ　）　ＦＦＦ）　ＧＧＧ　（ＨＨＨ　-”あああ　いいい”　ううう））　”　ＪＪＪ　”　-（ＫＫＫ　（　）　ＬＬＬ）　　（ＭＭＭ）　２２２　".into());
         assert_eq!(
             target.value(),
-            " ＡＡＡ (\"１１１ ＣＣＣ\" (( ＤＤＤ エエエ ) ＦＦＦ) ＧＧＧ (ＨＨＨ \"あああ いいい\" ううう)) \" ＪＪＪ \" (ＫＫＫ ( ) ＬＬＬ)  (ＭＭＭ) ２２２ "
+            " ＡＡＡ (\"１１１ ＣＣＣ\" (-( ＤＤＤ エエエ ) ＦＦＦ) ＧＧＧ (ＨＨＨ -\"あああ いいい\" ううう)) \" ＪＪＪ \" -(ＫＫＫ ( ) ＬＬＬ)  (ＭＭＭ) ２２２ "
         )
     }
 
     #[test]
     fn test_layered_by_bracket() {
         let target =
-            Query::new("　ＡＡＡ　（”１１１　ＣＣＣ”　（（　ＤＤＤ　エエエ　）　ＦＦＦ）　ＧＧＧ　（ＨＨＨ　”あああ　いいい”　ううう））　”　ＪＪＪ　”　（ＫＫＫ　（　）　ＬＬＬ）　　（ＭＭＭ）　２２２　".into());
+            Query::new("　ＡＡＡ　（”１１１　ＣＣＣ”　（-（　ＤＤＤ　エエエ　）　ＦＦＦ）　ＧＧＧ　（ＨＨＨ　-”あああ　いいい”　ううう））　”　ＪＪＪ　”　-（ＫＫＫ　（　）　ＬＬＬ）　　（ＭＭＭ）　２２２　".into());
         assert_eq!(
             target.layered_by_bracket().unwrap(),
             LayeredQueries(vec![
@@ -147,20 +177,20 @@ mod tests {
                 LayeredQuery::Bracket(LayeredQueries(vec![
                     LayeredQuery::Query(Query::new("\"１１１ ＣＣＣ\" ".into())),
                     LayeredQuery::Bracket(LayeredQueries(vec![
-                        LayeredQuery::Bracket(LayeredQueries(vec![LayeredQuery::Query(
+                        LayeredQuery::NegativeBracket(LayeredQueries(vec![LayeredQuery::Query(
                             Query::new(" ＤＤＤ エエエ ".into())
                         ),])),
                         LayeredQuery::Query(Query::new(" ＦＦＦ".into())),
                     ])),
                     LayeredQuery::Query(Query::new(" ＧＧＧ ".into())),
                     LayeredQuery::Bracket(LayeredQueries(vec![LayeredQuery::Query(Query::new(
-                        "ＨＨＨ \"あああ いいい\" ううう".into()
+                        "ＨＨＨ -\"あああ いいい\" ううう".into()
                     )),]))
                 ])),
                 LayeredQuery::Query(Query::new(" \" ＪＪＪ \" ".into())),
-                LayeredQuery::Bracket(LayeredQueries(vec![LayeredQuery::Query(Query::new(
-                    "ＫＫＫ  ＬＬＬ".into()
-                )),])),
+                LayeredQuery::NegativeBracket(LayeredQueries(vec![LayeredQuery::Query(
+                    Query::new("ＫＫＫ  ＬＬＬ".into())
+                ),])),
                 LayeredQuery::Bracket(LayeredQueries(vec![LayeredQuery::Query(Query::new(
                     "ＭＭＭ".into()
                 )),])),
@@ -170,37 +200,93 @@ mod tests {
     }
 
     #[test]
-    fn test_parse() {
-        let target =
-            Query::new("　ＡＡＡ　（”１１１　ＣＣＣ”　（（　ＤＤＤ　エエエ　）　ＦＦＦ）　ＧＧＧ　（ＨＨＨ　”あああ　いいい”　ううう））　”　ＪＪＪ　”　（ＫＫＫ　（　）　ＬＬＬ）　　（ＭＭＭ）　２２２　".into());
+    fn test_query_parse_to_condition() {
+        let target = Query::new("　ＡＡＡ　and　-ＢＢＢ　ＣＣＣ　or　ＤＤＤ　and　\"　ＥＥＥ　and　ＦＦＦ　\"　and　-\"　ＧＧＧ　or　ＨＨＨ　\"　ＩＩＩ　".into());
         assert_eq!(
-            target.layered_by_bracket().unwrap().parse().unwrap(),
-            Condition::Or(vec![
-                Condition::Keyword("ＡＡＡ".into()),
-                Condition::Or(vec![
-                    Condition::ExactKeyword("１１１ ＣＣＣ".into()),
-                    Condition::Or(vec![
-                        Condition::Or(vec![
+            target.parse_to_condition().unwrap(),
+            Condition::Operator(
+                Operator::Or,
+                vec![
+                    Condition::Operator(
+                        Operator::And,
+                        vec![
+                            Condition::Keyword("ＡＡＡ".into()),
+                            Condition::Not(Box::new(Condition::Keyword("ＢＢＢ".into())))
+                        ]
+                    ),
+                    Condition::Keyword("ＣＣＣ".into()),
+                    Condition::Operator(
+                        Operator::And,
+                        vec![
                             Condition::Keyword("ＤＤＤ".into()),
-                            Condition::Keyword("エエエ".into()),
-                        ]),
-                        Condition::Keyword("ＦＦＦ".into()),
-                    ]),
-                    Condition::Keyword("ＧＧＧ".into()),
-                    Condition::Or(vec![
-                        Condition::Keyword("ＨＨＨ".into()),
-                        Condition::ExactKeyword("あああ いいい".into()),
-                        Condition::Keyword("ううう".into()),
-                    ]),
-                ]),
-                Condition::ExactKeyword(" ＪＪＪ ".into()),
-                Condition::Or(vec![
-                    Condition::Keyword("ＫＫＫ".into()),
-                    Condition::Keyword("ＬＬＬ".into()),
-                ]),
-                Condition::Or(vec![Condition::Keyword("ＭＭＭ".into()),]),
-                Condition::Keyword("２２２".into()),
-            ])
+                            Condition::ExactKeyword("ＥＥＥ and ＦＦＦ".into()),
+                            Condition::Not(Box::new(Condition::ExactKeyword(
+                                "ＧＧＧ or ＨＨＨ".into()
+                            )))
+                        ]
+                    ),
+                    Condition::Keyword("ＩＩＩ".into()),
+                ]
+            )
+        )
+    }
+
+    #[test]
+    fn test_layered_queries_parse_to_condition() {
+        let target =
+            Query::new("　ＡＡＡ　（”１１１　ＣＣＣ”　（-（　ＤＤＤ　エエエ　）　ＦＦＦ）　ＧＧＧ　（ＨＨＨ　-”あああ　いいい”　ううう））　”　ＪＪＪ　”　-（ＫＫＫ　（　）　ＬＬＬ）　　（ＭＭＭ）　２２２　".into());
+        assert_eq!(
+            target
+                .layered_by_bracket()
+                .unwrap()
+                .parse_to_condition()
+                .unwrap(),
+            Condition::Operator(
+                Operator::Or,
+                vec![
+                    Condition::Keyword("ＡＡＡ".into()),
+                    Condition::Operator(
+                        Operator::Or,
+                        vec![
+                            Condition::ExactKeyword("１１１ ＣＣＣ".into()),
+                            Condition::Operator(
+                                Operator::Or,
+                                vec![
+                                    Condition::Not(Box::new(Condition::Operator(
+                                        Operator::Or,
+                                        vec![
+                                            Condition::Keyword("ＤＤＤ".into()),
+                                            Condition::Keyword("エエエ".into()),
+                                        ]
+                                    ))),
+                                    Condition::Keyword("ＦＦＦ".into()),
+                                ]
+                            ),
+                            Condition::Keyword("ＧＧＧ".into()),
+                            Condition::Operator(
+                                Operator::Or,
+                                vec![
+                                    Condition::Keyword("ＨＨＨ".into()),
+                                    Condition::Not(Box::new(Condition::ExactKeyword(
+                                        "あああ いいい".into()
+                                    ))),
+                                    Condition::Keyword("ううう".into()),
+                                ]
+                            ),
+                        ]
+                    ),
+                    Condition::ExactKeyword(" ＪＪＪ ".into()),
+                    Condition::Not(Box::new(Condition::Operator(
+                        Operator::Or,
+                        vec![
+                            Condition::Keyword("ＫＫＫ".into()),
+                            Condition::Keyword("ＬＬＬ".into()),
+                        ]
+                    ))),
+                    Condition::Operator(Operator::Or, vec![Condition::Keyword("ＭＭＭ".into()),]),
+                    Condition::Keyword("２２２".into()),
+                ]
+            )
         )
     }
 }
